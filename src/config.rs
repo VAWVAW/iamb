88f03495b16daa0aa7a46a6e06f4c32d6f4d96e3
vaw <git@nlih.de>
2025-09-13 12:56:52 +0200

    Add Message info

--- src/config.rs
+++ src/config.rs
@@ -1015,7 +1015,7 @@ impl Tunables {
         }
     }
 
-    fn values(self) -> TunableValues {
+    pub fn values(self) -> TunableValues {
         TunableValues {
             encryption: self.encryption.values(),
             proxy: self.proxy.unwrap_or_default().values(),
