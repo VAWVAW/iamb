81c155b333fe6b5ea53cc25efada2977f23da6e6
vaw <git@nlih.de>
2026-08-22 23:39:42 +0200

    Add `:set message_time_display`

--- src/config.rs
+++ src/config.rs
@@ -1423,6 +1423,8 @@ pub enum TunablesUpdate {
     #[strum_discriminants(strum(props(is_bool = true)))]
     TypingNoticeDisplay(bool),
     #[strum_discriminants(strum(props(is_bool = true)))]
+    MessageTimeDisplay(bool),
+    #[strum_discriminants(strum(props(is_bool = true)))]
     MessageUserColor(bool),
     #[strum_discriminants(strum(props(is_bool = true)))]
     Ignorecase(bool),
@@ -1620,6 +1622,8 @@ impl TunablesUpdate {
             "notypingnoticesend" => Self::TypingNoticeSend(false),
             "typingnoticedisplay" => Self::TypingNoticeDisplay(true),
             "notypingnoticedisplay" => Self::TypingNoticeDisplay(false),
+            "messagetimedisplay" => Self::MessageTimeDisplay(true),
+            "nomessagetimedisplay" => Self::MessageTimeDisplay(false),
             "messageusercolor" => Self::MessageUserColor(true),
             "nomessageusercolor" => Self::MessageUserColor(false),
             "ignorecase" => Self::Ignorecase(true),
@@ -2677,6 +2681,9 @@ impl ApplicationSettings {
             },
             TunablesUpdate::InputPrompt(prompt) => {
                 self.tunables.input_prompt = prompt;
+            },
+            TunablesUpdate::MessageTimeDisplay(message_time_display) => {
+                self.tunables.message_time_display = message_time_display;
             },
         }
     }
